cb31fc81310b9a5936ea1ffafe059f35a409029e
wwwemmanueljacob-png <www.emmanueljacob@gmail.com>
2026-09-07 18:48:44 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import crypto from "crypto";
-
+import testSupabaseRouter from "./testSupabase.js";
 dotenv.config();
 
 const app = express();
