5aa19fbcc0ecfd8b4fbee0a5a69b2de62403a39f
wwwemmanueljacob-png <www.emmanueljacob@gmail.com>
2026-09-12 22:46:45 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2055,7 +2055,7 @@ app.get(
         ),
 
         fetchAllRows(
-          "schedules",
+          "loan_schedules",
           "id,created_at,loan_id,customer_id,installment_number,due_date,amount_due,amount_paid,remaining_amount,status,paid_date"
         ),
 
